c2e4cec80ec7a729790db3171e0e4f149c9f862f
IsaacGalindoAmaya <36777618+IsaacGalindoAmaya@users.noreply.github.com>
2018-02-23 17:59:39 -0600

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Estoy agregando el codigo de mi programa");
-		
+		System.out.println("Estoy agregando un codido desde GitHub");
 	}
 }
