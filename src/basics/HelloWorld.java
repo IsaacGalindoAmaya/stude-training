d0836b126b84aa4dcb54d26b44b903245a5d491b
ChaiAmaya <iscamaya@hotmail.com>
2018-02-23 17:55:24 -0600

    modified Hellor World app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Estoy agregando el codigo de mi programa");
 		
 	}
 }
